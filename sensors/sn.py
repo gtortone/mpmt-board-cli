587b00c4f01dc47a8ccb43b1ae8b833785c9ee32
Gennaro Tortone <gennaro.tortone@na.infn.it>
2021-07-06 11:51:39 +0200

    introducing external sensor (BME or BMP)

--- sensors/sn.py
+++ sensors/sn.py
@@ -18,20 +18,44 @@ from bme280 import BME280
 from bmx160 import BMX160
 from tla2024 import TLA2024
 
+def ansi_print(text):
+   cmd2.ansi.style_aware_write(sys.stdout, text + '\n')
+
+def printHeader():
+   print('')
+   label = ['+5.0V','+3.3V','IpoeA','IpoeB','PpoeA','PpoeB','T','P','H']
+   unit = ['[V]','[V]','[A]','[A]','[W]','[W]','[°C]','[hPa]','[%Rh]']
+   if(bmeExt is not None):
+      label.extend(['Text','Pext'])
+      unit.extend(['[°C]','[hPa]'])
+      if(bmeExt.readId() == 0x96):      # BME280
+         label.append('Hext')
+         unit.append('[%Rh]')
+   if(args.bmx):
+      label.extend(['Magn X/Y/Z','Gyro X/Y/Z','Accel X/Y/Z'])
+      unit.extend(['[uT]','[g]','[m/s^2]'])
+
+   ansi_print(bright_cyan(st.generate_data_row(label)))
+   ansi_print(bright_blue(st.generate_data_row(unit)))
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--continuous', action='store_true', help='continuous mode', default=False)
 parser.add_argument('--bmx', action='store_true', help='include BMX160 metrics', default=False)
 args = parser.parse_args()
 
 # Text styles used in the data
-bright_black = functools.partial(cmd2.ansi.style, fg=cmd2.ansi.fg.bright_black)
-bright_yellow = functools.partial(cmd2.ansi.style, fg=cmd2.ansi.fg.bright_yellow)
-bright_green = functools.partial(cmd2.ansi.style, fg=cmd2.ansi.fg.bright_green)
-bright_red = functools.partial(cmd2.ansi.style, fg=cmd2.ansi.fg.bright_red)
 bright_cyan= functools.partial(cmd2.ansi.style, fg=cmd2.ansi.fg.bright_cyan)
 bright_blue = functools.partial(cmd2.ansi.style, fg=cmd2.ansi.fg.bright_blue)
-yellow = functools.partial(cmd2.ansi.style, fg=cmd2.ansi.fg.yellow)
-blue = functools.partial(cmd2.ansi.style, fg=cmd2.ansi.fg.bright_blue)
+
+# BME280 on-board sensor
+bme = BME280(1, 0x76)
+
+# BME280 external sensor
+bmeExt = BME280(1, 0x77)
+try:
+   bmeExt.readId()
+except IOError:
+   bmeExt = None
 
 columns: List[Column] = list()
 columns.append(Column("", width=5, data_horiz_align=HorizontalAlignment.RIGHT))  # 5.0V
@@ -41,28 +65,19 @@ columns.append(Column("", width=5, data_horiz_align=HorizontalAlignment.RIGHT)) 
 columns.append(Column("", width=5, data_horiz_align=HorizontalAlignment.RIGHT))  # PpoeA
 columns.append(Column("", width=5, data_horiz_align=HorizontalAlignment.RIGHT))  # PpoeB
 columns.append(Column("", width=6, data_horiz_align=HorizontalAlignment.RIGHT))  # T
-columns.append(Column("", width=6, data_horiz_align=HorizontalAlignment.RIGHT))  # P
+columns.append(Column("", width=7, data_horiz_align=HorizontalAlignment.RIGHT))  # P
 columns.append(Column("", width=6, data_horiz_align=HorizontalAlignment.RIGHT))  # H
+if(bmeExt is not None):
+   columns.append(Column("", width=6, data_horiz_align=HorizontalAlignment.RIGHT))  # Text
+   columns.append(Column("", width=7, data_horiz_align=HorizontalAlignment.RIGHT))  # Pext
+   if(bmeExt.readId() == 0x96):      # BME280
+      columns.append(Column("", width=6, data_horiz_align=HorizontalAlignment.RIGHT))  # Hext
 if(args.bmx):
    columns.append(Column("", width=16, data_horiz_align=HorizontalAlignment.RIGHT))  # Magn X/Y/Z
    columns.append(Column("", width=16, data_horiz_align=HorizontalAlignment.RIGHT))  # Gyro X/Y/Z
    columns.append(Column("", width=16, data_horiz_align=HorizontalAlignment.RIGHT))  # Accel X/Y/Z
 
 st = SimpleTable(columns, divider_char=None)
-
-def ansi_print(text):
-   cmd2.ansi.style_aware_write(sys.stdout, text + '\n')
-
-def printHeader():
-   print('')
-   label = ['+5.0V','+3.3V','IpoeA','IpoeB','PpoeA','PpoeB','T','P','H']
-   unit = ['[V]','[V]','[A]','[A]','[W]','[W]','[°C]','[hPa]','[%Rh]']
-   if(args.bmx):
-      label.extend(['Magn X/Y/Z','Gyro X/Y/Z','Accel X/Y/Z'])
-      unit.extend(['[uT]','[g]','[m/s^2]'])
-
-   ansi_print(bright_cyan(st.generate_data_row(label)))
-   ansi_print(bright_blue(st.generate_data_row(unit)))
 
 i = 0
 while (True):
@@ -88,11 +103,17 @@ while (True):
    row.append(f'{P1:.3f}')
    row.append(f'{P2:.3f}')
 
-   bme = BME280(1, 0x76)
    bmeData = bme.readAll()
    row.append(f'{bmeData[0]:.2f}')
    row.append(f'{bmeData[1]:.2f}')
    row.append(f'{bmeData[2]:.2f}')
+
+   if(bmeExt is not None):
+      bmeData = bmeExt.readAll()
+      row.append(f'{bmeData[0]:.2f}')
+      row.append(f'{bmeData[1]:.2f}')
+      if(bmeExt.readId() == 0x96):      # BME280
+         row.append(f'{bmeData[2]:.2f}')
 
    if(args.bmx):
       bmx = BMX160(1, 0x69)
@@ -105,7 +126,7 @@ while (True):
 
    i = i + 1
 
-   time.sleep(0.5)
+   time.sleep(1)
 
    if(not args.continuous):
       break

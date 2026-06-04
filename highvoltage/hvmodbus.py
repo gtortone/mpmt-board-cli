a5cb1f4aa0dd8a9ff83f64f3fac7785bf6d37f16
Gennaro Tortone <gennaro.tortone@na.infn.it>
2026-06-04 10:03:17 +0000

    hv: restore pymodbus 3.9.2 methods

--- highvoltage/hvmodbus.py
+++ highvoltage/hvmodbus.py
@@ -40,7 +40,7 @@ class HVModbus:
    @rpc_method
    def open(self, addr) -> int:
       try:
-         rr = self.client.read_holding_registers(address=0, count=1, device_id=addr)
+         rr = self.client.read_holding_registers(address=0, count=1, slave=addr)
       except ModbusException as e:
          #print(e)
          return False
@@ -52,47 +52,47 @@ class HVModbus:
       
    @rpc_method
    def getStatus(self, slave) -> int:
-      rr = self.client.read_holding_registers(address=6, count=1, device_id=slave)
+      rr = self.client.read_holding_registers(address=6, count=1, slave=slave)
       return rr.registers[0]
 
    @rpc_method 
    def getVoltage(self, slave) -> float:
-      rr = self.client.read_holding_registers(address=0x2A, count=2, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x2A, count=2, slave=slave)
       rr.registers.reverse()
       return self.client.convert_from_registers(rr.registers, data_type=self.client.DATATYPE.INT32) / 1000
 
    @rpc_method
    def getVoltageSet(self, slave) -> int:
-      rr = self.client.read_holding_registers(address=0x26, count=1, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x26, count=1, slave=slave)
       return rr.registers[0]
 
    @rpc_method
    def setVoltageSet(self, value: float, slave):
-      self.client.write_register(address=0x26, value=value, device_id=slave)
+      self.client.write_register(address=0x26, value=value, slave=slave)
 
    @rpc_method
    def getCurrent(self, slave) -> float:
-      rr = self.client.read_holding_registers(address=0x28, count=2, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x28, count=2, slave=slave)
       rr.registers.reverse()
       return self.client.convert_from_registers(rr.registers, data_type=self.client.DATATYPE.INT32) / 1000
 
    @rpc_method
    def getTemperature(self, slave) -> float:
-      rr = self.client.read_holding_registers(address=0x7, count=1, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x7, count=1, slave=slave)
       return self.convertTemperature(rr.registers[0])
 
    @rpc_method
    def getRateRampup(self,  slave) -> int:
-      rr = self.client.read_holding_registers(address=0x23, count=2, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x23, count=2, slave=slave)
       return rr.registers[0]   
 
    @rpc_method
    def getRateRampdown(self,  slave) -> int:
-      rr = self.client.read_holding_registers(address=0x23, count=2, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x23, count=2, slave=slave)
       return rr.registers[1]   
 
    #def getRate(self, fmt=str, slave) -> str:
-   #   rr = self.client.read_holding_registers(address=0x23, count=2, device_id=slave)
+   #   rr = self.client.read_holding_registers(address=0x23, count=2, slave=slave)
    #   rup = rr.registers[0]
    #   rdn = rr.registers[1]
    #   if fmt == str:
@@ -102,14 +102,14 @@ class HVModbus:
 
    @rpc_method
    def setRateRampup(self, value, slave):
-      self.client.write_register(address=0x23, value=value, device_id=slave)
+      self.client.write_register(address=0x23, value=value, slave=slave)
 
    @rpc_method
    def setRateRampdown(self, value, slave):
-      self.client.write_register(address=0x24, value=value, device_id=slave)
+      self.client.write_register(address=0x24, value=value, slave=slave)
 
    #def getLimit(self, fmt=str, slave) -> float:
-   #   rr = self.client.read_holding_registers(address=0, count=48, device_id=slave)
+   #   rr = self.client.read_holding_registers(address=0, count=48, slave=slave)
    #   lv = rr.registers[0x27]
    #   li = rr.registers[0x25]
    #   lt = rr.registers[0x2F]
@@ -122,68 +122,68 @@ class HVModbus:
 
    @rpc_method
    def setLimitVoltage(self, value, slave):
-      self.client.write_register(address=0x27, value=value, device_id=slave)
+      self.client.write_register(address=0x27, value=value, slave=slave)
 
    @rpc_method
    def setLimitCurrent(self, value, slave):
-      self.client.write_register(address=0x25, value=value, device_id=slave)
+      self.client.write_register(address=0x25, value=value, slave=slave)
 
    @rpc_method
    def setLimitTemperature(self, value, slave):
-      self.client.write_register(address=0x2F, value=value, device_id=slave)
+      self.client.write_register(address=0x2F, value=value, slave=slave)
 
    @rpc_method
    def setLimitTriptime(self, value, slave):
-      self.client.write_register(address=0x22, value=value, device_id=slave)
+      self.client.write_register(address=0x22, value=value, slave=slave)
 
    @rpc_method
    def setThreshold(self, value, slave):
-      self.client.write_register(address=0x2D, value=math.floor(value), device_id=slave)
-      self.client.write_register(address=0x35, value=int(value * 10) % 10, device_id=slave)
+      self.client.write_register(address=0x2D, value=math.floor(value), slave=slave)
+      self.client.write_register(address=0x35, value=int(value * 10) % 10, slave=slave)
 
    @rpc_method
    def getThreshold(self, slave) -> float:
       print("getThreshold")
-      ri = self.client.read_holding_registers(address=0x2D, count=1, device_id=slave)
-      rf = self.client.read_holding_registers(address=0x35, count=1, device_id=slave)
+      ri = self.client.read_holding_registers(address=0x2D, count=1, slave=slave)
+      rf = self.client.read_holding_registers(address=0x35, count=1, slave=slave)
       return ri.registers[0] + rf.registers[0]/10
 
    @rpc_method
    def getAlarm(self, slave) -> int:
-      rr = self.client.read_holding_registers(address=0x2E, count=1, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x2E, count=1, slave=slave)
       return rr.registers[0]
 
    @rpc_method
    def getVref(self, slave) -> float:
-      rr = self.client.read_holding_registers(address=0x2C, count=1, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x2C, count=1, slave=slave)
       return rr.registers[0]/10
 
    @rpc_method
    def powerOn(self, slave):
-      rr = self.client.write_coil(address=1, value=True, device_id=slave)
+      rr = self.client.write_coil(address=1, value=True, slave=slave)
       return not rr.isError()
 
    @rpc_method
    def powerOff(self, slave):
-      rr = self.client.write_coil(address=1, value=False, device_id=slave)
+      rr = self.client.write_coil(address=1, value=False, slave=slave)
       return not rr.isError()
 
    @rpc_method
    def reset(self, slave):
-      rr = self.client.write_coil(address=2, value=True, device_id=slave)
+      rr = self.client.write_coil(address=2, value=True, slave=slave)
       return not rr.isError()
 
    @rpc_method
    def getInfo(self, slave):
-      l = self.client.read_holding_registers(address=0x02, count=1, device_id=slave).registers
+      l = self.client.read_holding_registers(address=0x02, count=1, slave=slave).registers
       fwver = struct.pack(f'>{len(l)}h', *l).decode()
-      l = self.client.read_holding_registers(address=0x08, count=6, device_id=slave).registers
+      l = self.client.read_holding_registers(address=0x08, count=6, slave=slave).registers
       pmtsn = struct.pack(f'>{len(l)}h', *l).decode()
-      l = self.client.read_holding_registers(address=0x0E, count=6, device_id=slave).registers
+      l = self.client.read_holding_registers(address=0x0E, count=6, slave=slave).registers
       hvsn = struct.pack(f'>{len(l)}h', *l).decode()
-      l = self.client.read_holding_registers(address=0x14, count=6, device_id=slave).registers
+      l = self.client.read_holding_registers(address=0x14, count=6, slave=slave).registers
       febsn = struct.pack(f'>{len(l)}h', *l).decode()
-      l = self.client.read_holding_registers(address=0x04, count=2, device_id=slave).registers
+      l = self.client.read_holding_registers(address=0x04, count=2, slave=slave).registers
       devid = (l[1] << 16) + l[0]
       return fwver, pmtsn, hvsn, febsn, devid
 
@@ -191,28 +191,28 @@ class HVModbus:
    def setPMTSerialNumber(self, sn: str, slave):
       data = self.client.convert_to_registers(sn.ljust(12, '\0'), self.client.DATATYPE.STRING)
       self.client.write_registers(address=0x08, values=data,
-        device_id=slave, no_response_expected=True)
+        slave=slave, no_response_expected=True)
 
    @rpc_method
    def setHVSerialNumber(self, sn: str, slave):
       data = self.client.convert_to_registers(sn.ljust(12, '\0'), self.client.DATATYPE.STRING)
       self.client.write_registers(address=0x0E, values=data,
-        device_id=slave, no_response_expected=True)
+        slave=slave, no_response_expected=True)
 
    @rpc_method
    def setFEBSerialNumber(self, sn: str, slave):
       data = self.client.convert_to_registers(sn.ljust(12, '\0'), self.client.DATATYPE.STRING)
       self.client.write_registers(address=0x14, values=data,
-        device_id=slave, no_response_expected=True)
+        slave=slave, no_response_expected=True)
 
    @rpc_method
    def setModbusAddress(self, addr):
-      self.client.write_register(address=0x00, value=addr, device_id=slave)
+      self.client.write_register(address=0x00, value=addr, slave=slave)
 
    @rpc_method
    def readMonRegisters(self, slave):
       monData = {}
-      rr = self.client.read_holding_registers(address=0, count=54, device_id=slave)
+      rr = self.client.read_holding_registers(address=0, count=54, slave=slave)
 
       if rr.isError():
          return None
@@ -242,7 +242,7 @@ class HVModbus:
 
    @rpc_method
    def readCalibRegisters(self, slave):
-      rr = self.client.read_holding_registers(address=0x30, count=5, device_id=slave)
+      rr = self.client.read_holding_registers(address=0x30, count=5, slave=slave)
       mlsb = rr.registers[0]
       mmsb = rr.registers[1]
       qlsb = rr.registers[2]
@@ -266,16 +266,16 @@ class HVModbus:
       slope = int(slope * 10000)
       lsb = (slope & 0xFFFF)
       msb = (slope >> 16) & 0xFFFF
-      self.client.write_registers(address=0x30, values=[lsb, msb], device_id=slave, no_response_expected=True)
+      self.client.write_registers(address=0x30, values=[lsb, msb], slave=slave, no_response_expected=True)
 
    @rpc_method
    def writeCalibOffset(self, offset: float, slave):
       offset = int(offset * 10000)
       lsb = (offset & 0xFFFF)
       msb = (offset >> 16) & 0xFFFF
-      self.client.write_registers(address=0x32, values=[lsb, msb], device_id=slave, no_response_expected=True)
+      self.client.write_registers(address=0x32, values=[lsb, msb], slave=slave, no_response_expected=True)
 
    @rpc_method
    def writeCalibDiscr(self, discr: float, slave):
       discr = int(discr * 1.6890722)
-      self.client.write_register(address=0x34, value=discr, device_id=slave, no_response_expected=True)
+      self.client.write_register(address=0x34, value=discr, slave=slave, no_response_expected=True)

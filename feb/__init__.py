3368586fa9b5dab06c123a03bd6c764a91cc0170
Gennaro Tortone <gennaro.tortone@na.infn.it>
2026-06-18 08:53:43 +0000

    feb library moved to mpmt-mss repository

--- feb/__init__.py
+++
@@ -1,5 +0,0 @@
-from .feb_manager import FEBManager
-from .devices import DeviceType, DeviceConfig, DeviceChannel
-from .pmtchannel import PMTChannel
-from .ledchannel import LEDChannel
-from .modbus_manager import ModbusManager, ModbusConfig

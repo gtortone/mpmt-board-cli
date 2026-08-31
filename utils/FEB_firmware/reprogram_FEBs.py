22b17f9b20f6a4b1d298643bc1cb76b75e687f74
Gennaro Tortone <gennaro.tortone@na.infn.it>
2026-08-31 09:25:09 +0000

    improve reprogram_FEBs.py

--- utils/FEB_firmware/reprogram_FEBs.py
+++ utils/FEB_firmware/reprogram_FEBs.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import mmap
 import subprocess
@@ -16,27 +18,26 @@ def write(add: int, value: int) -> None:
     global regs
     regs[add*4:(add*4)+4] = int.to_bytes(value, 4, byteorder='little')
 
-
 def read(add: int) -> int:
     global regs
     return int.from_bytes(regs[add*4:(add*4)+4], byteorder='little')
 
-
 def program_feb(addr: int, firmware:str) -> None:
     print(f"Turning on channel {addr}")
-    write(0, 2 ** (int(addr)-1))
+    write(0, (1<<(addr-1)))
     time.sleep(1)
-    write(1, 2 ** (int(addr)-1))
+    write(1, (1<<(addr-1)))
     time.sleep(3)
 
     print(f'Start programming FEB {addr}')
-    subprocess.run(['stm32flash',  '-b', '115200', '-w', f'{firmware}', '-e', '255', '-v', '/dev/ttyPS1'], shell=False)
+    res = subprocess.run(['stm32flash',  '-b', '115200', '-w', f'{firmware}', '-e', '255', '-v', '/dev/ttyPS1'], shell=False)
 
     time.sleep(1)
     write(0, 0)
     time.sleep(1)
     write(1, 0)
 
+    return res.returncode
 
 def main(FEBnum: str, firmwarename: str) -> None:
     if FEBnum == 'all':
@@ -48,8 +49,12 @@ def main(FEBnum: str, firmwarename: str) -> None:
         febnum = FEBnum.split(",")
 
     for i in febnum:
-        program_feb(int(i), firmwarename)
-        print(f'FEB {i} programmed')
+        res = program_feb(int(i), firmwarename)
+        if res != 0:
+            print(f'FEB {i} programming FAILED')
+            print(f'(ModBus services running ? (mpmt-mss, ...)')
+        else:
+            print(f'FEB {i} programming OK')
 
 
 if __name__ == '__main__':
